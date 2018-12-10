f75d8ffd07f4856186011a619012124889bf6d22
Josip Kovačević <38467885+jkovacevic992@users.noreply.github.com>
2018-12-10 16:51:11 +0100

    Playing with numbers and letters

--- If, Else, Elseif/submit.php
+++ If, Else, Elseif/submit.php
@@ -8,21 +8,49 @@
 	
 	if($_POST["input_text"]>0){
 	
-		echo $_POST["input_text"]. " is positive.";
+		echo $_POST["input_text"]. " is positive.</br>";
 	
 	
 	}elseif($_POST["input_text"]<0){
-		echo $_POST["input_text"]. " is negative.";
+		echo $_POST["input_text"]. " is negative.</br>";
 	}else{
 		
-		echo $_POST["input_text"]. " is neither positive nor negative.";
+		echo $_POST["input_text"]. " is neither positive nor negative.</br>";
 	}
 	?>
-	<form action="positive_or_negative.php" method="POST">
+	<form action="user_input_practice.php" method="POST">
 	<input type="submit" name="return" value="Return">
 	
 	
 	</form>
+	
+	<?php
+	
+	
+	$vowels = "";
+	$input = strtolower($_POST["input1"]);
+	
+	if($input == "a" 
+	|| $input == "e"
+	|| $input == "i"
+	|| $input == "o"
+	|| $input == "u"
+	|| $input == "y"){
+		
+		$vowels = true;
+	}else{
+		
+		$vowels = false;
+	}
+	if($vowels){
+		echo $_POST["input1"] . " is a vowel.";
+		
+	}else{
+		echo $_POST["input1"] . " is a consonant.";
+	}
+	
+	
+	?>
 	
 	</body>
 </html>
--- If, Else, Elseif/user_input_practice.php
+++ If, Else, Elseif/user_input_practice.php
@@ -8,7 +8,13 @@
 	<p>Enter a number and I'll tell you if it is positive or negative</p>
 		<form action="submit.php" method="POST">
 	<input type="text" name = "input_text" placeholder="Enter a number"/>
-	<input type="submit" name = "submit_button" />
+	<input type="submit" name = "submit1" />
+	
+	
+	<p>Enter a letter and I'll tell you if it is a vowel or a consonant</p>
+		<form action="submit.php" method="POST">
+	<input type="text" name = "input1" placeholder="Enter a letter"/>
+	<input type="submit" name = "submit2" />
 	</form>
 	</body>
 </html>
