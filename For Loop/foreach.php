7832ac43746a1b68ad883d708a968e9704d88696
Josip Kovačević <38467885+jkovacevic992@users.noreply.github.com>
2018-12-20 16:41:04 +0100

    Learning to use the foreach loop

---
+++ For Loop/foreach.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Foreach</title>
+	</head>
+	<body>
+		<?php 
+	
+	$friends = array("John", "Mary", "Mark", "Simon", "Garfunkel", "Stipe", "Ivan", "Carl");
+	
+	foreach ($friends as $value) {
+		
+		
+		echo "<h1>$value is my friend. </h1>";
+		
+	}
+
+
+
+
+		?>
+	</body>
+</html>
