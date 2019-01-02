ac3e4026109138d6570a826207b060e2c0ad63d2
Josip Kovačević <38467885+jkovacevic992@users.noreply.github.com>
2019-01-02 16:04:50 +0100

    Exercise 3 finished

---
+++ Functions/functions_3.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Functions 3</title>
+	</head>
+	<body>
+	
+	
+	<?php
+	//Write a function to reverse a string.
+		
+	function reverseString($i){
+		
+		
+		
+		return strrev($i);;
+	}
+	
+	echo reverseString("This is a reversed string.");
+	
+	?>
+	</body>
+</html>
